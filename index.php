59dbd37545b4dccc42371da8b9037157eaa273a7
BaluSP21 <147434562+BaluSP21@users.noreply.github.com>
2023-10-25 17:08:13 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title>Timão XAMPP</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel = "stylesheet" type = "text/css" href="style.css">
+
+</head>
+<body>
+	<header>
+		<h1>Nação Corinthiana</h1>
+	</header>
+
+	<main>
+
+		<h2>A História do Timão</h2>
+
+		<section class="mundial">
+
+			<p style="line-height: 1.5;">O Sport Club Corinthians Paulista, comumente conhecido como Corinthians, é um clube poliesportivo brasileiro da cidade de São Paulo. Foi fundado em 1º de setembro de 1910 por um grupo de operários do bairro do Bom Retiro. O nome do clube foi inspirado no Corinthian FC de Londres, que estava fazendo uma excursão pelo Brasil na época.<br>
+
+			O primeiro presidente escolhido foi o alfaiate Miguel Battaglia, que afirmou: “O Corinthians vai ser o time do povo e o povo é quem vai fazer o time”. O clube começou a jogar em um terreno alugado na Rua José Paulino, que foi aplainado e transformado em campo. O primeiro treino ocorreu já no dia 14 de setembro.<br>
+
+			Em 1911, o Corinthians jogou seu primeiro jogo fora da cidade de São Paulo, vencendo a Ponte Preta em Campinas por 1 a 0. Em 1913, o clube participou pela primeira vez do campeonato da Liga Paulista de Futebol. Em 1914, conquistou seu primeiro título com 10 vitórias em 10 jogos disputados.<br>
+
+			Desde 2014, o Corinthians manda suas partidas de futebol na Neo Química Arena. Seus rivais históricos são o Palmeiras, com quem disputa o Derby Paulista; o São Paulo, com quem disputa o Majestoso; e o Santos, com quem disputa o Clássico Alvinegro. Sua torcida é conhecida como "Fiel" e seus torcedores são estimados em aproximadamente 30 milhões espalhados por todo o Brasil e pelo mundo.
+			<center>
+				
+				<img src="https://logodetimes.com/times/corinthians/logo-corinthians-4096.png" alt="Sport Club Corinthians Paulista">
+
+			</center>
+	
+
+			
+			<h2>Títulos</h2>
+
+            <p style="line-height: 1.5;"><br>
+                <table border="1" class="center-table">
+                    <tr>
+                        <td>
+                            <table >
+                                <tr>
+                                    <td>
+                                        - 2x Mundial de Clubes FIFA<br>
+                                        - 1x Taça Libertadores da América<br>
+                                        - 1x Recopa Sul-Americana<br>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table >
+                                <tr>
+                                    <td>
+                                        - 7x Campeonato Brasileiro<br>
+                                        - 3x Copa do Brasil<br>
+                                        - 1x Supercampeonato Brasileiro<br>
+
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                        <td>
+                            <table >
+                                <tr>
+                                    <td>
+                                    	- 1x Campeonato Brasileiro Série B<br>
+                                        - 5x Torneio Rio-São Paulo<br>
+                                        - 30x Campeonato Paulista
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </p>
+        </section>
+    </main>
+</body>
+</html>
+
